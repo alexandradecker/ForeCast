47745339f2d9e35dfd79aa8fc1090b41ffd8eb47
ribhav99 <ribhav.kapur99@gmail.com>
2020-11-30 22:04:31 -0500

    update

--- pca.py
+++ pca.py
@@ -4,13 +4,21 @@ PCA analysis to find importance of various features.
 from train_test_split import my_train_test_split
 from sklearn.decomposition import PCA as pca
 import numpy as np
+import pandas as pd
 
-x_train, x_test, y_train, y_test, for_pca = my_train_test_split(1)
+x_train, x_test, y_train, y_test, for_pca = my_train_test_split()
 
 model = pca().fit(for_pca)
 num_components = model.components_.shape[0]
 
 most_important = [np.abs(model.components_[i]).argmax()
                   for i in range(num_components)]
+
+columns = pd.read_csv("bigTable/allParticipantsTable.csv").columns.tolist()
+names = []
+for i in most_important:
+    names.append(columns[i])
+
 print(most_important)
-print(model.components_)
+print(names)
+# print(model.components_)
--- train_test_split.py
+++ train_test_split.py
@@ -11,8 +11,8 @@ def my_train_test_split(participant=None):
         df = pd.read_csv("bigTable/allParticipantsTable.csv")
     else:
         df = pd.read_csv("bigTable/"+str(participant)+".csv")
-    df = df[['max(RIGHT_FIX_INDEX)', 'avg(RIGHT_IN_BLINK)', 'avg(RIGHT_IN_SACCADE)', 'avg(RIGHT_PUPIL_SIZE)', 'max_pupil', 'avg_preceding_pupil', 'avg_pupil', 'change_pupil',
-             'rt', 'stimnum', 'acc', 'rtResidual', 'rtResidualC', 'rt_rec', 'trialno_rec', 'ratingConfidence', 'pupil_deviation', 'rt_deviation', 'acc_rec']]
+    #removed 'max_pupil', 'avd_preceding_pupil', 'avg_pupil', 'change_pupil', 'rt', 'rtResidual', 'rtResidualC'
+    df = df[['max(RIGHT_FIX_INDEX)', 'avg(RIGHT_IN_BLINK)', 'avg(RIGHT_IN_SACCADE)', 'avg(RIGHT_PUPIL_SIZE)', 'stimnum', 'acc', 'rt_rec', 'trialno_rec', 'ratingConfidence', 'pupil_deviation', 'rt_deviation', 'acc_rec']]
     df.dropna(inplace=True)
     data = np.array(df.values.tolist())
 
@@ -22,7 +22,7 @@ def my_train_test_split(participant=None):
     y = []
 
     for i in range(len(x1)):
-        if len(x1[i]) == 18:  # CHANGE VALUE DEPENDING ON NUM OF COLUMNS IN X
+        if len(x1[i]) == 11:  # CHANGE VALUE DEPENDING ON NUM OF COLUMNS IN X
             x.append(x1[i])
             y.append(y1[i])
     x = np.asarray(x)
